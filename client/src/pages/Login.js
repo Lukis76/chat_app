96f7ecf6d38df7bb407c661f5cefd65c6adcf8ce
lukis76 <kiu.manoguerra@gmail.com>
2022-08-15 16:31:20 -0300

    react-hot-toast

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,5 @@
 import React from 'react'
 
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-
 export const Login = () => {
-  const notify = () => toast('Wow so easy!')
-
-  return (
-    <div>
-      <button onClick={notify}>Notify!</button>
-      <ToastContainer />
-    </div>
-  )
+  return <div>Login</div>
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,8 @@
-import React, { Fragment, useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { SvgHermes } from '../assets/SvgHermes'
-// import { ToastContainer, toast } from 'react-toastify'
-// import 'react-toastify/dist/ReactToastify.css'
+import toast from 'react-hot-toast'
 
 export const Register = () => {
   const [values, setValues] = useState({
@@ -13,34 +12,25 @@ export const Register = () => {
     confirmPassword: ''
   })
 
-  // const optionsToast = {
-  //   position: 'top-right',
-  //   autoClose: 5000,
-  //   closeOnClick: true,
-  //   pauseOnHover: true,
-  //   draggable: true,
-  //   theme: 'dark'
-  // }
-
   const handleSubmit = (e) => {
     e.preventDefault()
-    // handleValidation()
+    handleValidation()
     console.log('submit')
   }
 
-  // const handleValidation = () => {
-  //   const { username, email, password, confirmPassword } = values
-  //   if (username.length < 6) {
-  //     return 'Username must be at least 3 characters'
-  //   } else if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
-  //     return 'Email is not valid'
-  //   } else if (password.length < 8) {
-  //     return 'Password must be at least 6 characters'
-  //   } else if (password !== confirmPassword) {
-  //     // toast('Password does not match', optionsToast)
-  //     return 'Passwords do not match'
-  //   }
-  // }
+  const handleValidation = () => {
+    const { username, email, password, confirmPassword } = values
+    if (username.length < 6) {
+      return 'Username must be at least 3 characters'
+    } else if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
+      return 'Email is not valid'
+    } else if (password.length < 8) {
+      return 'Password must be at least 6 characters'
+    } else if (password !== confirmPassword) {
+      toast('passwod incorrecto gil...')
+      return 'Passwords do not match'
+    }
+  }
 
   const handleChange = (e) => {
     console.log(e.target.name, ':', e.target.value)
@@ -48,47 +38,44 @@ export const Register = () => {
   }
 
   return (
-    <Fragment>
-      <FromConteiner>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <Head>
-            <SvgHermes />
-            <h1>Hermes</h1>
-          </Head>
-          <input
-            type="text"
-            placeholder="UserName"
-            name="username"
-            autoComplete="on"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            autoComplete="on"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="password"
-            placeholder="ConfirmPassword"
-            name="confirmPassword"
-            onChange={(e) => handleChange(e)}
-          />
-          <button type="submit">Create User</button>
-          <span>
-            already have an account ? <Link to="/login">Login</Link>
-          </span>
-        </form>
-      </FromConteiner>
-      {/* <ToastContainer /> */}
-    </Fragment>
+    <FromConteiner>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <Head>
+          <SvgHermes />
+          <h1>Hermes</h1>
+        </Head>
+        <input
+          type="text"
+          placeholder="UserName"
+          name="username"
+          autoComplete="on"
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          autoComplete="on"
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          placeholder="ConfirmPassword"
+          name="confirmPassword"
+          onChange={(e) => handleChange(e)}
+        />
+        <button type="submit">Create User</button>
+        <span>
+          already have an account ? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </FromConteiner>
   )
 }
 
